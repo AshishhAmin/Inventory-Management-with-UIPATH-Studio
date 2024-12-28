c3a729ea36ac8da93faca31be146835ac444417c
Ashishcodes <150023259+AshishhAmin@users.noreply.github.com>
2024-12-28 13:10:37 +0530

    Update settings.py

--- inventorymanagement/settings.py
+++ inventorymanagement/settings.py
@@ -25,7 +25,9 @@ SECRET_KEY = "django-insecure-)(8keq*gvbw_0a6ea+et)n1ne)yqd=7hs2d-9_g60j64co-5^2
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = [
+    'inventorymanagement-eight.vercel.app',
+]
 
 
 # Application definition
